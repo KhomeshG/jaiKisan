ac38834d769a3058175adf10279c04b99df12535
KhomeshG <khomesshgajbhiye@gmail.com>
2023-02-07 01:39:27 +0530

    Done All

--- src/controller/cardController.js
+++ src/controller/cardController.js
@@ -1,8 +1,9 @@
 const cardSchema = require("../module/cartSchema");
+const { findOneAndUpdate } = require("../module/custermerSchema");
 
 module.exports.getCard = async (req, res) => {
   try {
-    const result = await cardSchema.find().select({ cardNumber: 1 });
+    const result = await cardSchema.find().select({ __v: 0 });
 
     res.status(200).send({ status: true, msg: "Sucess", data: result });
   } catch (err) {
@@ -13,8 +14,19 @@ module.exports.getCard = async (req, res) => {
 };
 module.exports.createCard = async (req, res) => {
   try {
-    let num = 1;
-    let data = { cardNumber: "C" + (0 + 0 + 1 + num++) };
+    const data = { cardNumber: "C003" };
+    if (req.body.status) {
+      data.status = req.body.status;
+    }
+    if (req.body.customerName) {
+      data.customerName = req.body.customerName;
+    }
+    if (req.body.vision) {
+      data.vision = req.body.vision;
+    }
+    if (req.body.customerID) {
+      data.customerID = req.body.customerID;
+    }
 
     const result = await cardSchema.create(data);
 
